6119054b85dd10db7656d5b4bba03c23e40b115d
kdj <88639900+djgohigh@users.noreply.github.com>
2022-12-22 10:49:43 +0900

    planetscale, prisma2

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,11 @@ import '../styles/globals.css'
 import css from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
-  //pageProps = { 'param' : '안녕하세요'};
-  // pageProps = '민경환'
   return (
-    <div className={css.scene}>
-        ㅇㄹㅇㄹ
-    </div>
+    // Component : 호출된 페이지
+    // _app.js 에서는 공통된 레이아웃을 적용시킬 수 있다. 네비, 푸터 등등 변동되는 요소는 컴포넌트 관리
+    <Component className="h-full">
+    </Component>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home(pageProps) {
-  return (
-    <h1 className="text-3xl font-bold underline text-center text-emerald-500">에메랄드 밑줄 안녕하세요</h1>
-  )
+    let greet = '반가워요!\n우주인이 되어보세요 🚀';
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <style global jsx>
+                {`
+                    div#__next > div {
+                        height : 100vh;
+                    }`
+                }
+            </style>
+            <div className="w-3/12">
+                <h1 className="mb-5 text-3xl text-center font-extrabold bg-gradient-to-r text-transparent bg-clip-text to-emerald-600 from-sky-400 whitespace-pre-line">{greet}</h1>
+                <form>
+                    <div className="grid gap-3 mb-6">
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">아이디</label>
+                            <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="아이디" required></input>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">비밀번호</label>
+                            <input type="password" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="비밀번호" required></input>
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이름</label>
+                            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="이름" required></input>
+                        </div>  
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">이메일</label>
+                            <input type="text" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="이메일" required></input>
+                        </div>  
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="w-36 mx-auto text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">등록하기</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        
+    );
 }
